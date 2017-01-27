1989929ae8a59ece61a644d6ba95868b1203d114
vigneswardn <vigneswardn@gmail.com>
2017-01-28 04:24:23 +0530

    commit

--- src/main/java/com/phase1/rest/BlogController.java
+++ src/main/java/com/phase1/rest/BlogController.java
@@ -1,21 +1,48 @@
 package com.phase1.rest;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.phase1.api.dto.Blog;
+import com.phase1.biz.BloggerImpl;
+
 @Path("/blog")
 public class BlogController {
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/blog/{id}")
-	public Response read(@PathParam("id") int number) {
-		return Response.ok().build();
+	@Path("getBlog/{blogId}")
+	public Response getBlog(@PathParam("blogId") String blogId) {
+		BloggerImpl impl = new BloggerImpl();
+		Blog blog = new Blog();
+		blog.setBlogId(Integer.valueOf(blogId));
+		impl.getBlog(blog);
+		return Response.ok().entity(blog).build();
 	}
-
 	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/addBlog/")
+	public Response addBlog(Blog blog) {
+		BloggerImpl impl = new BloggerImpl();
+		impl.addBlog(blog);
+		return Response.ok().entity(blog).build();
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/updateBlog/")
+	public Response updateBlog(Blog blog) {
+		BloggerImpl impl = new BloggerImpl();
+		impl.updateBlog(blog);
+		return Response.ok().entity(blog).build();
+	}
 }
